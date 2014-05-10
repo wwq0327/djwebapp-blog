50047389a6d7437a5c1b9d2e8f1a234c252b3243
wwq0327 <wwq0327@gmail.com>
2014-05-10 11:37:55 +0800

    urls

--- djblog/blog/views.py
+++ djblog/blog/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render_to_response
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response, get_list_or_404, get_object_or_404
 from django.template import RequestContext
 
+from blog.models improt Post
+
 def index(request):
-	return render_to_response("index.html",
-							  context_instance=RequestContext(request))
+	"""blog列表"""
+	
+	posts = get_list_or_404(Post)
+	return render_to_response("blog/index.html",
+				  {"posts": posts},
+				  context_instance=RequestContext(request))
+
+def post(request, pk):
+	"""单篇文章"""
+	
+	post = get_object_or_404(Post, pk=pk)
+	return render_to_response("blog/post.html",
+				  {"post": post},
+				  context_instance=RequestContext(request))
--- djblog/djblog/urls.py
+++ djblog/djblog/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    #url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', 'blog.views.index', name="index"),
+                       url(r'^$', include('blog.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
 )
