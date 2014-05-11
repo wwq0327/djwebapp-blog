0bfcf19c993a8ed177caf52cb4d0b3f1032363ec
wwq0327 <wwq0327@gmail.com>
2014-05-11 11:16:22 +0800

    模块修改

--- djblog/blog/models.py
+++ djblog/blog/models.py
@@ -13,4 +13,7 @@ class Post(models.Model):
 
     def __unicode__(self):
         return self.title
- 
+    
+    @models.permalink
+    def get_absolute_url(self):
+        return ('post', (), {'pk': self.pk}) 
--- djblog/djblog/urls.py
+++ djblog/djblog/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', include('blog.urls')),
+                       url(r'^', include('blog.urls')),
                        url(r'^admin/', include(admin.site.urls)),
 )
