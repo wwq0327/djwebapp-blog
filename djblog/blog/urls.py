a3fbd28d3c0c8251aac74a903095647237f0fd67
wwq0327 <wwq0327@gmail.com>
2014-05-11 10:28:31 +0800

    f

--- djblog/blog/urls.py
+++ djblog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-                       url(r"^$", "blog.views.index", name="blog:index"),
-                       url(r"^post/(?P<pk>\d+)/$", "blog.views.post", name="blog:post"),
+                       url(r"^$", "blog.views.index", name="index"),
+                       url(r"^post/(?P<pk>\d+)/$", "blog.views.post", name="post"),
 )                       
